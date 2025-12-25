3339c9bd532996a69b83d0d36017db66a8c41da5
Manoj <manojsrinivaskottur@gmail.com>
2025-12-25 11:04:03 +0530

    minor fix

--- src/app/transport/transport-navigation/transport-navigation.component.spec.ts
+++ src/app/transport/transport-navigation/transport-navigation.component.spec.ts
@@ -1,19 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Router } from '@angular/router';
 import { TransportNavigationComponent } from './transport-navigation.component';
 import { TransportService } from '../transport.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TransportNavigationComponent', () => {
   let component: TransportNavigationComponent;
   let fixture: ComponentFixture<TransportNavigationComponent>;
-  let mockRouter: any;
   let mockTransportService: any;
 
   beforeEach(async () => {
-    mockRouter = {
-      navigate: jest.fn()
-    };
-
     mockTransportService = {
       initializeRiderDetail: jest.fn().mockReturnValue({}),
       RIDE_TAB: 'Ride',
@@ -22,9 +17,9 @@ describe('TransportNavigationComponent', () => {
     };
 
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ TransportNavigationComponent ],
       providers: [
-        { provide: Router, useValue: mockRouter },
         { provide: TransportService, useValue: mockTransportService }
       ]
     })
@@ -41,16 +36,5 @@ describe('TransportNavigationComponent', () => {
 
   it('should initialize rider details on init', () => {
     expect(mockTransportService.initializeRiderDetail).toHaveBeenCalled();
-  });
-
-  it('should set default tab to Ride on init', () => {
-     expect(component.selectedTab).toBe('Ride');
-     expect(mockRouter.navigate).toHaveBeenCalledWith(['/ride']);
-  });
-
-  it('should change navigation bar item', () => {
-    component.onChangeNavBarItem('Drop');
-    expect(component.selectedTab).toBe('Drop');
-    expect(mockRouter.navigate).toHaveBeenCalledWith(['/drop']);
   });
 });
--- src/app/transport/transport-navigation/transport-navigation.component.ts
+++ src/app/transport/transport-navigation/transport-navigation.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TransportService } from '../transport.service';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-transport-navigation',
@@ -9,22 +8,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class TransportNavigationComponent implements OnInit {
 
-  selectedTab: string = '';
 
   riderDetail: any;
 
-  constructor(public transportService: TransportService, private router: Router) {}
+  constructor(public transportService: TransportService) {}
 
   // Initialize component and rider details
   ngOnInit() {
     this.riderDetail = this.transportService.initializeRiderDetail();
-    this.onChangeNavBarItem(this.transportService.RIDE_TAB);
-  }
-
-  // Handle tab selection change
-  onChangeNavBarItem(tab: string) {
-    this.selectedTab = tab;
-    this.router.navigate(['/' + tab.toLowerCase()]);
   }
 
 }
